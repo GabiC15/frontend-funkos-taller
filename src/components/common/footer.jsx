1965c83eab8af821fe36c9c0ac3e4f2d8c7eb9e3
Gabriel <luisgabrielcasares@gmail.com>
2023-11-29 20:23:21 -0300

    Changes icon

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,4 +1,3 @@
-import logo from "@/assets/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,7 +9,13 @@ export default function Footer() {
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <Link href="/" className="flex items-center">
-                <Image src={logo} className="h-8 me-3" alt="Logo" />
+                <Image
+                  src="/logo.svg"
+                  width={200}
+                  height={8}
+                  className="h-8 me-3"
+                  alt="Logo"
+                />
               </Link>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
