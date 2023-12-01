c587dcd740886bd1387d2d4b1374f5a994cf0571
Gabriel <luisgabrielcasares@gmail.com>
2023-12-01 17:06:14 -0300

    Add components to page

--- src/pages/productos/index.jsx
+++ src/pages/productos/index.jsx
@@ -1,9 +1,12 @@
 import Layout from "@/components/common/layout";
+import ProductosSection from "@/components/productos/productos";
 
 export default function Productos() {
   return (
     <>
-      <Layout>Productos</Layout>
+      <Layout>
+        <ProductosSection />
+      </Layout>
     </>
   );
 }
