50abbdce9b164beb85558666476514684580c63b
Gabriel <luisgabrielcasares@gmail.com>
2023-12-21 21:17:03 -0300

    Add Pedido components to page

---
+++ src/pages/usuario/pedido.jsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/common/layout";
+import NuevoPedidoSection from "@/components/pedido/nuevo/NuevoPedido";
+
+export default function NuevoPedido() {
+  return (
+    <>
+      <Layout>
+        <NuevoPedidoSection />
+      </Layout>
+    </>
+  );
+}
