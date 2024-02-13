6fba53517957e27bcd97ccf717b531b7aa595951
Gabriel <luisgabrielcasares@gmail.com>
2024-02-12 23:22:58 -0300

    Add favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,8 +6,10 @@ import { ApolloProvider } from "@apollo/client";
 import client from "@/services/apollo/client";
 import UserProvider from "@/components/providers/UserProvider";
 import CarritoProvider from "@/components/providers/CarritoProvider";
+import Head from "next/head";
 // import { initMercadoPago } from "@mercadopago/sdk-react";
 config.autoAddCss = false;
+import favicon from "../static/favicon.ico";
 
 const font = Noto_Sans({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -19,6 +21,9 @@ const font = Noto_Sans({
 export default function App({ Component, pageProps }) {
   return (
     <div className={font.className}>
+      <Head>
+        <link rel="shortcut icon" href={favicon.src} />
+      </Head>
       <ApolloProvider client={client}>
         <UserProvider>
           <CarritoProvider>
