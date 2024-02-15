73c85bbe1e2ed300c3933fb6858dc98aeff120f9
Donato De Battista <doni.db@hotmail.com>
2024-02-14 21:52:52 -0300

    add-emailJS-toForm

--- src/components/home/formulario.jsx
+++ src/components/home/formulario.jsx
@@ -1,11 +1,33 @@
+import {useRef} from 'react'
+import emailjs from '@emailjs/browser';
 import Image from "next/image";
 
 export default function Formulario() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_dy531ev', 'template_ly7ksjg', form.current, {
+        publicKey: 'snHso6b8EsjFJe8G7',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
     <>
       <div className="bg-[url(/home/formulario-bg.png)] bg-cover w-full">
         <div className="container mx-auto flex flex-col md:flex-row justify-center items-center md:items-between py-12 md:py-20 px-4 md:px-0 gap-10 md:gap-40">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div className="flex flex-col w-full md:w-[30rem]">
               <h4 className="text-xl font-bold">ESCRIBENOS POR CONSULTAS!</h4>
               <label
@@ -17,6 +39,7 @@ export default function Formulario() {
               <input
                 type="text"
                 id="first_name"
+                name='first_name'
                 className="bg-transparent border-2 border-white text-white text-sm rounded-lg focus:ring-white block w-full p-2.5"
                 placeholder="Ingrese su nombre"
                 required
@@ -30,6 +53,7 @@ export default function Formulario() {
               <input
                 type="text"
                 id="mail"
+                name='mail'
                 className="bg-transparent border-2 border-white text-white text-sm rounded-lg focus:ring-white block w-full p-2.5"
                 placeholder="Ingrese su email"
                 required
@@ -42,6 +66,7 @@ export default function Formulario() {
               </label>
               <textarea
                 id="mensaje"
+                name='mensaje'
                 rows="5"
                 cols="33"
                 className="bg-transparent border-2 border-white text-white text-sm rounded-lg focus:ring-white block w-full p-2.5 max-h-36"
